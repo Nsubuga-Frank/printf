51323165cfee65cfb802db49d039bf6439579abc
Nsubuga-Frank <nsubugank@gmail.com>
2022-04-18 13:17:26 -0700

    project

--- _printf.c
+++ _printf.c
@@ -1,14 +1,12 @@
-#include"main.h"
-
-#include<stdio.h>
-
-#include<stdarg.h>
-
-#include<unistd.h>
-
-#include<stdlib.h>
+#include "main.h"
 
 
+
+/**
+ * _printf - custom function that format and print data
+ * @format:  list of types of arguments passed to the function
+ * Return: int
+ */
 
 
 
@@ -16,74 +14,130 @@ int _printf(const char *format, ...)
 
 {
 
-  va_list list;
+	va_list list;
 
-  unsigned int i = 0, j = 0;
+	int idx, j;
 
-  va_start(list, format);
+	int len_buf = 0;
+
+	char *s;
+
+	char *create_buff;
 
 
 
-  if (!format || (format[0] == '%' && format[1] == '\0'))
+	type_t ops[] = {
 
-    return (-1);
+		{"c", print_c},
 
-  for (i = 0; format != NULL && format[i] != '\0'; i++)
+		{"s", print_s},
 
-    {
+		{"i", print_i},
 
-      if (format[i] == '%')
+		{"d", print_i},
 
-	{
+		{"b", print_bin},
 
-	  if (format[i + 1] == '%')
+		{NULL, NULL}
 
-	    {
+	};
 
-	      _putchar('%');
 
-	      j++;
 
-	      i++;
+	create_buff = malloc(1024 * sizeof(char));
 
-	    }
-
-	  else if (_typefor(format, i + 1) != NULL)
-
-	    {
-
-	      j += _typefor(format, i + 1)(list);
-
-	      i++;
-
-	    }
-
-	  else
-
-	    {
-
-	      _putchar(format[i]);
-
-	      j++;
-
-	    }
-
-	}
-
-      else
+	if (create_buff == NULL)
 
 	{
 
-	  _putchar(format[i]);
+		free(create_buff);
 
-	  j++;
+		return (-1);
 
 	}
 
-    }
+	va_start(list, format);
 
-  va_end(list);
+	if (format == NULL || list == NULL)
 
-  return (j);
+		return (-1);
+
+	for (idx = 0; format[idx] != '\0'; idx++)
+
+	{
+
+		if (format[idx] == '%' && format[idx + 1] == '%')
+
+			continue;
+
+		else if (format[idx] == '%')
+
+		{
+
+			if (format[idx + 1] == ' ')
+
+				idx += _position(format, idx);
+
+			for (j = 0; ops[j].f != NULL; j++)
+
+			{
+
+				if (format[idx + 1] == *(ops[j].op))
+
+				{
+
+					s = ops[j].f(list);
+
+					if (s == NULL)
+
+						return (-1);
+
+					_strlen(s);
+
+					_strcat(create_buff, s, len_buf);
+
+					len_buf += _strlen(s);
+
+					idx++;
+
+					break;
+
+				}
+
+			}
+
+			if (ops[j].f == NULL)
+
+			{
+
+				create_buff[len_buf] = format[idx];
+
+				len_buf++;
+
+			}
+
+		}
+
+		else
+
+		{
+
+			create_buff[len_buf] = format[idx];
+
+			len_buf++;
+
+		}
+
+	}
+
+	create_buff[len_buf] = '\0';
+
+	write(1, create_buff, len_buf);
+
+	va_end(list);
+
+	free(create_buff);
+
+	return (len_buf);
 
 }
